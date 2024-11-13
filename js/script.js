b62ad413c0948350c7e99c544ec92ae6b89840ff
mykolamieshkov-web <mykolamieshkov@gmail.com>
2024-11-13 20:04:56 +0200

    layout and styles are ready

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+function openNav() {  // define the element by #id(0px of width by default); give it with while open;
+    document.getElementById("burger").style.display = "flex";
+  }
+  
+function closeNav() {  // define the same element by #id. Give it 0px of width (hide) after clicking on button
+    document.getElementById("burger").style.display = "none"; 
+}
